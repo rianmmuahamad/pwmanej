6fc696f407af4e26f3bd9df720739e0021d9d62d
rianmmuahamad <163876666+rianmmuahamad@users.noreply.github.com>
2025-05-17 22:17:25 +0700

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -21,7 +21,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL || '/auth/google/callback'
+  callbackURL: 'https://pwmanej.vercel.app/auth/google/callback'
 },
 async (accessToken, refreshToken, profile, done) => {
   try {
