eb6f677b3e7b21b76fc584aaa5cbabaca4165d2b
rianmmuahamad <rn453679@gmail.com>
2025-05-17 20:43:35 +0700

    baruuu

--- src/auth.js
+++ src/auth.js
@@ -8,9 +8,13 @@ passport.deserializeUser((user, done) => done(null, user));
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  callbackURL: process.env.VERCEL ? 'https://pwmanej.vercel.app/auth/google/callback' : 'http://localhost:3000/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
+  console.log('Google OAuth Response:', { accessToken, profile });
   return done(null, profile);
+}, (error, req, res, next) => {
+  console.error('Google OAuth Error:', error);
+  return next(error);
 }));
 
 module.exports = passport;
